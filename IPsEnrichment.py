a5df27a841062363ba8c3deb8fb89e12286f4ead
Nir Yosha <nir.yosha@threatq.com>
2018-07-19 08:17:18 -0400

    Update and rename 3Octets-k-means.py to IPsEnrichment.py

--- IPsEnrichment.py
+++ IPsEnrichment.py
@@ -1,9 +1,5 @@
-from copy import deepcopy
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-plt.rcParams['figure.figsize'] = (16, 9)
-plt.style.use('ggplot')
 
 # Importing the dataset
 data = pd.read_csv('xclara.csv')

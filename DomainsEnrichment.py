aec83ac14ef8922d8cc3a611d875478e1612dca7
Nir Yosha <nir.yosha@threatq.com>
2018-07-19 09:27:32 -0400

    Update DomainsEnrichment.py

--- DomainsEnrichment.py
+++ DomainsEnrichment.py
@@ -1,31 +1,46 @@
 # open raw data source file (IPs)
 import numpy as np
-from sklearn.cluster import MeanShift# as ms
-from sklearn.datasets.samples_generator import make_blobs
-import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import csv
 
-infile = "/root/OSINT/Plot/datasets/ThreatQIPs.csv"
-outfile = "/root/OSINT/Plot/datasets/ThreatQSplitIPs.csv"
+data = pd.read_csv('MaliciousIPs.csv')
+print(data.shape)
+data.head()
+data.desctibe()
+data[['octet1','octet2','octet3','octet4']] = data['IP'].str.split('.',expand=True)
+data[‘octet1’].value_counts()
 
-with open(outfile, "a") as out:
-    with open(infile) as f:
-        content = f.readlines()
-        # change IP to 3 chunks for 1-16,17-24,25-32 bits
-        for ip in content:
-            #print ip
-            binip = ''.join([bin(int(x)+256)[3:] for x in ip.split('.')])
-            splitbin = binip[0:16]
-            f1 = int(splitbin,2) # feature one is decimal of /24 network
-            f2 = int(ip.split('.')[2]) # feature three is octet 3
-            f3 = int(ip.split('.')[3])  # feature three is octet 4
-            out.write("%i,%i,%i\n" % (f1,f2,f3))
-            #print f1,f2,f3
-            #np.append([f1,f2,f3],axis=0)
-    f.close()
-out.close()
 
-# True
 
-    #print np
+reader = csv.reader(open('assignedIPv4blocks.csv', 'r'))
+d = {}
+for row in reader:
+   k, v = row
+   d[k] = v
+
+
+data[‘owner'] = data['octet1'].map(d)
+
+data['owner'].value_counts(normalize=True)
+
+data[['octet1','octet2']] = data[['octet1','octet2']].apply(pd.to_numeric)
+
+data['decimal'] = data['octet1']*256+data['octet2']
+
+
+reader2 = csv.reader(open('countryIPv4blocks.csv', 'r'))
+c = {}
+for row in reader2:
+   k, v = row
+   c[k] = v
+
+
+data['decimal'] = data['decimal'].apply(str)
+
+data['country'] = data['decimal'].map(c)
+
+data['country'].value_counts()
+
 
 
